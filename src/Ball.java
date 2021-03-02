c6293b17b6619e1ec332c82191700f6675806878
Renan Cardoso <renancardoso1910@gmail.com>
2021-03-02 19:00:48 -0300

    commitando referente a aula de atributos

--- src/Ball.java
+++ src/Ball.java
@@ -3,6 +3,16 @@ public class Ball {
 //    atributos
     String name = "";
 
+    /* Ver print informativo: tipo-primitivo-classe-wrapper.png
+     * atributos primitivos:
+     * int, short, long, float, double, char, boolean e etc..
+     * atributos das classes Wrappers:
+     * Boolean, Integer, String, Float e etc..
+     */
+
 //    métodos são ações
 
+    Boolean kick(){
+        return true;
+    }
 }
